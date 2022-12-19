affd232bd9cb5436600c5aff7dc44c0175d422d7
2bevantys <2001vantys@gmail.com>
2022-12-19 15:04:51 +0300

    patch for workflows

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import sqlite3 as sq
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
 
+
+class ExlEngApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, Words", halign="center")
+
+
+if __name__ == "__main__":
+    ExlEngApp().run()
